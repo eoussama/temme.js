c10d2ceae97735bb1fd75c166cb2152bf0a4fe1b
EOussama <oussama_gtacj@live.fr>
2018-05-12 22:30:39 +0100

    Initial commit

---
+++ scripts/temme.js
@@ -0,0 +1,7 @@
+/*
+    Title: 			TemmeJS
+    Version: 		0.1.0
+    Author: 		Eoussama
+    Description: 	Temme (or Emmet in reverse) is to javascript what Emmet is to HTML and CSS, with no doubts, Emmet saved us from the headache of working with HTML and CSS, and now, Temme is doing Javascript the same favour too.
+    License:		MIT
+*/
