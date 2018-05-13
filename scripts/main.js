4b8021f136a887227d985d3353094301bf34697f
EOussama <oussama_gtacj@live.fr>
2018-05-13 16:59:59 +0000

    main.js ex edited

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', () => {
     const target = document.getElementById("target");
 
-    Temme("div#container>h1.lead-3+div#footer>div.flex.flex-gap-3>p.indent", target);
+    Temme("h1>span+h2", target);
 });
