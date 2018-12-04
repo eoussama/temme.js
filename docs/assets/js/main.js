a590a94b71a7c93ee6da2b213c501c38724ef0f2
Eoussama <oussama_gtacj@live.fr>
2018-12-04 20:29:06 +0000

    gulp scripts added.

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -15,5 +15,26 @@
 window.addEventListener('load', () => {
     const target = document.getElementById('target');
 
-    Temme(['ff'], target);
+    Temme({
+        children: [
+            {
+                name: 'h1',
+                text: 'Heading',
+                classes: ['heading-1', 'bold']
+            },
+            {
+                name: 'h3',
+                text: 'Subsssssss to pewdddddds'
+            },
+            {
+                name: 'div',
+                children: [
+                    {
+                        name: 'p',
+                        'text': 'lorem ipsum'
+                    }
+                ]
+            }
+        ]
+    }, target);
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,27 @@
 const
     gulp = require('gulp'),
+    babel = require('gulp-babel'),
+    rename = require('gulp-rename'),
     uglify = require('gulp-uglify');
 
-HTMLOptGroupElement.task('build', () => {
-    gulp.src(['src/*.js'])
-        .pipe(uglify())
-        .pipe(gulp.dest('dist'));
-
-    gulp.src(['src/*.js'])
+// Translating to es5 and copying the temme.js file to the dist folder.
+gulp.task('copy', () => {
+    gulp.src('src/*.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))        
         .pipe(gulp.dest('dist'));
 });
+
+// Translating to es, minifying and copying the temme.js file to the dist folder.
+gulp.task('minify', () => {
+    return gulp.src(['src/*.js'])
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', ['copy', 'minify']);
