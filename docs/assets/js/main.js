f234e8269d7852110baa6075f915816f5ee48def
Eoussama <oussama_gtacj@live.fr>
2018-12-05 00:30:29 +0000

    Added few descriptions to the docs.

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -41,7 +41,39 @@ window.addEventListener('load', () => {
                 ]
             },
             {
-                name: 'main'
+                name: 'main',
+                children: [
+                    {
+                        name: 'section',
+                        children: [
+                            {
+                                name: 'h3',
+                                text: 'Description'
+                            },
+                            {
+                                name: 'p',
+                                text: "Temme (or Emmet in reverse) is to javascript what Emmet is to HTML and CSS, with no doubts, Emmet saved us from the headache of working with HTML and CSS, and now, it's time for Javascript too."
+                            }
+                        ]
+                    },
+                    {
+                        name: 'section',
+                        children: [
+                            {
+                                name: 'h3',
+                                text: 'How does it work?'
+                            },
+                            {
+                                name: 'p',
+                                text: "With Temme, manipulating the DOM, specifically creating simple to complex HTML skeletons has never been simpler."
+                            },
+                            {
+                                name: 'p',
+                                text: "With javascript objects as the blueprint and you as the engineer, you can produce skyscrapers even, you only need to provide a concise hierarchy for your HTML wonder and let Temme take care of the rest. In fact, this whole web page is completely generated using Temme."
+                            }
+                        ]
+                    }
+                ]
             }
         ]
     }, target);
