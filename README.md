f6e6f71ce04c6972ab7e4c5f524b6bf2bf641a85
Eoussama <oussama_gtacj@live.fr>
2018-12-05 19:24:21 +0000

    Changes to the description.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Temme JS
 
-Temme (or Emmet in reverse) is to javascript what Emmet is to HTML and CSS, with no doubts, Emmet saved us from the headache of working with HTML and CSS, and now, it's time for Javascript too.
+Temme (or Emmet in reverse) is to javascript what Emmet is to HTML and CSS, with no doubts, Emmet saved us from the headache of working with HTML and CSS, and now, it's about time Javascript had the same quirk too.
 
 # Build
 
--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -94,7 +94,7 @@ window.addEventListener('load', async () => {
                             },
                             {
                                 name: 'p',
-                                text: "Temme (or Emmet in reverse) is to javascript what Emmet is to HTML and CSS, with no doubts, Emmet saved us from the headache of working with HTML and CSS, and now, it's time for Javascript too."
+                                text: "Temme (or Emmet in reverse) is to javascript what Emmet is to HTML and CSS, with no doubts, Emmet saved us from the headache of working with HTML and CSS, and now, it's about time Javascript had the same quirk too"
                             }
                         ]
                     },
