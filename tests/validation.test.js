2d3b09d952ac81a65564d38e1a001263b3fac9e9
Eoussama <oussama_gtacj@live.fr>
2018-12-11 15:26:08 +0000

    Added validation checks.

--- tests/validation.test.js
+++ tests/validation.test.js
@@ -12,13 +12,74 @@
  */
 
 
+const validator = require('../build/modules/validator');
 const options = require('../build/modules/options').options;
+
+
+describe('Target checks.', () => {
+
+    describe('Invalid target.', () => {
+
+        test('Checking if invalid target (undefined) throws an error.', () => {
+    
+            // Assert.
+            expect(validator.isValidHTMLElement(undefined)).toBe(false);
+        });
+    
+        test('Checking if invalid target (null) throws an error.', () => {
+            
+            // Assert.
+            expect(validator.isValidHTMLElement(null)).toBe(false);
+        });
+    
+        test('Checking if invalid target (boolean) throws an error.', () => {
+            
+            // Assert.
+            expect(validator.isValidHTMLElement(false)).toBe(false);
+        });
+    
+        test('Checking if invalid target (number) throws an error.', () => {
+            
+            // Assert.
+            expect(validator.isValidHTMLElement(1634)).toBe(false);
+        });
+    
+        test('Checking if invalid target (string) throws an error.', () => {
+            
+            // Assert.
+            expect(validator.isValidHTMLElement('fls')).toBe(false);
+        });
+    
+        test('Checking if invalid target (array) throws an error.', () => {
+            
+            // Assert.
+            expect(validator.isValidHTMLElement([])).toBe(false);
+        });
+    
+        test('Checking if invalid target (object) throws an error.', () => {
+            
+            // Assert.
+            expect(validator.isValidHTMLElement({})).toBe(false);
+        });
+    });
+
+    describe('Valid target.', () => {
+        test("Checking if invalid target (HTML element) doesn't throw any error.", () => {
+            // Arrange.
+            const target = document.createElement('div');
+
+            // Assert.
+            expect(validator.isValidHTMLElement(target)).toBe(true);
+        });
+    });
+});
 
 
 describe('Option checks.', () => {
 
-    test('Checking of all options are loaded.', () => {
+    test('Checking if all options are loaded.', () => {
 
+        // Assert.
         expect(options.length).toBe(12);
-    })
+    });
 });
